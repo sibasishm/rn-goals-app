f1b8739f379770954eece8db6339fbd3f45873b4
Sibasish Mohanty <smsibasish@gmail.com>
2020-07-12 15:21:28 +0530

    ScrollView intro

--- App.js
+++ App.js
@@ -1,32 +1,73 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	ScrollView,
+} from 'react-native';
 
 export default function App() {
 	const [inputGoal, setInputGoal] = useState('');
 	const [allGoals, setAllGoals] = useState([]);
 	return (
-		<View>
+		<View style={styles.conatiner}>
 			<View>
-				<Text>Goal Tracker</Text>
+				<Text style={styles.title}>Goal Tracker</Text>
 				<TextInput
+					style={styles.input}
 					placeholder='Add Goal'
 					value={inputGoal}
 					onChangeText={(text) => setInputGoal(text)}
 				/>
 				<Button
 					title='+ Add'
-					onPress={() => setAllGoals((prev) => prev.push(inputGoal))}
+					onPress={() => {
+						setAllGoals(() => [...allGoals, inputGoal]);
+						setInputGoal('');
+					}}
+					disabled={!inputGoal}
 				/>
 			</View>
-			<View>
+			<ScrollView style={styles.list}>
 				{allGoals.length > 0 ? (
 					allGoals.map((goal, index) => (
-						<Text key={`goal-${index}`}>{goal}</Text>
+						<Text style={styles.listItem} key={`goal-${index}`}>{`${
+							index + 1
+						}. ${goal}`}</Text>
 					))
 				) : (
 					<Text>No goals added yet.</Text>
 				)}
-			</View>
+			</ScrollView>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	conatiner: {
+		padding: 25,
+		marginTop: 25,
+		flex: 1,
+	},
+	title: {
+		fontSize: 30,
+		fontWeight: '700',
+		textAlign: 'center',
+	},
+	input: {
+		marginVertical: 5,
+		padding: 5,
+		borderBottomWidth: 1,
+		borderBottomColor: '#000',
+	},
+	list: {
+		marginVertical: 5,
+	},
+	listItem: {
+		fontSize: 16,
+		fontWeight: '400',
+		marginVertical: 2,
+	},
+});
